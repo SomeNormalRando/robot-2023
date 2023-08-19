3462e0523cd940c4e8d10ca9ca1bdb343d6d4f18
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2023-08-19 18:07:20 +0800

    sensors ultrasonic sensor & color sensor + latency displays improved logging (use builtin logging class)

--- robot/ev3.py
+++ robot/ev3.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 print("ev3.py started")
 
-from ev3dev2.display import Display
 import ev3dev2.fonts as fonts
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
-from datetime import datetime, timezone, timedelta
+import logging
+from datetime import datetime
+
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%H:%M:%S", level=logging.DEBUG)
 
 LEDs = Leds()
 LEDs.set_color("LEFT", "AMBER")
@@ -27,29 +29,9 @@ MQTT_BROKER_PORT = 8883
 #endregion
 
 #region Helper functions
-display = Display()
 spkr = Sound()
 def speak(text):
     spkr.speak(text, volume = 100, play_type = 1)
-
-def display_info(message: str, do_you_want_to_speak_it = False):
-    if (do_you_want_to_speak_it == True):
-        speak(message)
-
-    now = datetime.now(timezone(timedelta(hours=8))).strftime("%H:%M:%S")
-
-    print(str.format("[{}] {}", now, message))
-
-    '''
-    display.text_pixels(
-        text = str.format("[{}] {}", now, message),
-        clear_screen = True,
-        x = 0, y = 0,
-        text_color = "OrangeRed",
-        font = DISPLAY_FONT
-    )
-    display.update()
-    '''
 
 def clamp(num: float, numMin: float, numMax: float):
     return max(min(numMax, num), numMin)
@@ -66,8 +48,8 @@ def dc_clamp(value: float):
     return clamp(value, -MAX_LARGE_MOTOR_SPEED, MAX_LARGE_MOTOR_SPEED)
 #endregion
 
-display_info("Constants and helper functions loaded.")
-display_info("Loading modules...", False)
+logging.info("Constants and helper functions loaded.")
+logging.info("Loading modules...")
 
 import threading
 import evdev
@@ -76,24 +58,23 @@ from json import dumps as stringify_json
 from concurrent.futures import ThreadPoolExecutor
 
 from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
-import ev3dev2
-from ev3dev2 import motor
+from ev3dev2.sensor import Sensor, INPUT_2, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.power import PowerSupply
 
 import paho.mqtt.client as paho
 from paho import mqtt
 
 
-
-display_info("Modules loaded.")
+logging.info("Modules loaded.")
 
 #region Initialisation
-display_info("Finding PS4 controller...")
+logging.info("Finding PS4 controller...")
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 gamepad = devices[0]
 if gamepad.name != "Wireless Controller":
     raise ConnectionError(str.format("PS4 controller not found (found `{}` instead).", gamepad))
-display_info("PS4 controller found.")
+logging.info("PS4 controller found.")
 
 class MotorThread(threading.Thread):
     forward_speed = 0.0
@@ -103,23 +84,16 @@ class MotorThread(threading.Thread):
     publishing = True
 
     def __init__(self):
-
         self.power = PowerSupply("/sys/class/power_supply/", "lego-ev3-battery", True)
 
         self.left_motor = LargeMotor(OUTPUT_B)
         self.right_motor = LargeMotor(OUTPUT_C)
 
-        try:
-            self.pusher_motor = LargeMotor(OUTPUT_A)
-        except(ev3dev2.DeviceNotFound):
-            display_info("No large motor found at Port A (push motor). Proceeding without.")
-            pass
+        self.pusher_motor = LargeMotor(OUTPUT_A)
+        self.opener_motor = MediumMotor(OUTPUT_D)
 
-        try:
-            self.opener_motor = MediumMotor(OUTPUT_D)
-        except(ev3dev2.DeviceNotFound):
-            display_info("No medium motor found at Port D (open motor). Proceeding without.")
-            pass
+        self.color_sensor = ColorSensor(INPUT_2)
+        self.ultrasonic_sensor = Sensor(INPUT_4)
 
         self.init_mqtt()
 
@@ -138,60 +112,63 @@ class MotorThread(threading.Thread):
             self.right_motor.run_forever(
                 speed_sp = dc_clamp(self.forward_speed + self.side_speed)
             )
-            try:
-                self.opener_motor.run_forever(
-                    speed_sp = self.opener_motor_speed
-                )
-                if self.pusher_motor_running == True:
-                    self.pusher_motor.on_for_rotations(PUSHER_MOTOR_SPEED, PUSHER_MOTOR_ROTATION)
-                    self.pusher_motor_running = False
-            except motor.SpeedInvalid as e:
-                pass
+            self.opener_motor.run_forever(
+                speed_sp = self.opener_motor_speed
+            )
+            if self.pusher_motor_running == True:
+                self.pusher_motor.on_for_rotations(PUSHER_MOTOR_SPEED, PUSHER_MOTOR_ROTATION)
+                self.pusher_motor_running = False
 
     def init_mqtt(self):
         def on_connect(client, userdata, flags, rc, properties=None):
-            display_info(str.format("[MQTT] CONNACK received with code `{}`.", rc))
+            logging.info(str.format("[MQTT] CONNACK received with code `{}`.", rc))
         def on_publish(client, userdata, mid):
-            display_info(str.format("[MQTT] Message published.", mid))
+            logging.info(str.format("[MQTT] Message published.", mid))
 
         self.client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
         self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
         self.client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-        display_info("[MQTT] Connecting to broker...")
+        logging.info("[MQTT] Connecting to broker...")
         self.client.connect(MQTT_BROKER_ADDRESS, MQTT_BROKER_PORT)
 
         self.client.on_connect = on_connect
         self.client.on_publish = on_publish
 
+        self.client.enable_logger()
+
         self.client.loop_start()
-        display_info("[MQTT] Network loop started.")
+        logging.info("[MQTT] Network loop started.")
 
     def start_publishing(self):
+        logging.info("start_publishing")
         while True:
             if self.publishing == True:
                 message = self.create_message()
                 self.client.publish("ev3/test", payload=message, qos=1)
                 sleep(1)
-                
 
     def create_message(self):
         messageDict = {
+            # python timestamp is in seconds while JavaScript timestamp is in milliseconds
+            "timestamp": int(datetime.timestamp(datetime.now()) * 1000),
             "batteryLevel": self.power.measured_volts,
             "leftMotorSpeed": self.left_motor.speed,
             "rightMotorSpeed": self.right_motor.speed,
             "openerMotorSpeed": self.opener_motor.speed,
             "pusherMotorSpeed": self.pusher_motor.speed,
+            "detectedColour": self.color_sensor.color_name,
+            "height": self.ultrasonic_sensor.value(),
         }
         return stringify_json(messageDict)
 
-display_info("Initialising MotorThread class.")
+logging.info("Initialising MotorThread class.")
 mt = MotorThread()
 mt.setDaemon(True)
 mt.start()
 
-display_info("MotorThread started.")
-display_info("Robot fully initialised.", False)
+logging.info("MotorThread started.")
+logging.info("Robot fully initialised.")
 
 LEDs.set_color("LEFT", "GREEN")
 LEDs.set_color("RIGHT", "GREEN")
@@ -218,11 +195,13 @@ for event in gamepad.read_loop():   # this loops infinitely
             mt.side_speed = -MAX_LARGE_MOTOR_SPEED
         elif event.code == 305 and event.value == 1:
             if mt.publishing == True:
+                logging.info("[DEV] Stopped publishing.")
                 mt.publishing = False
-                LEDs.set_color("LEFT", "ORANGE")
-                LEDs.set_color("RIGHT", "ORANGE")
+                LEDs.set_color("LEFT", "YELLOW")
+                LEDs.set_color("RIGHT", "YELLOW")
             else:
                 mt.publishing = True
+                logging.info("[DEV] Started publishing again.")
                 LEDs.set_color("LEFT", "GREEN")
                 LEDs.set_color("RIGHT", "GREEN")
         else:
--- site/public/script.js
+++ site/public/script.js
@@ -7,20 +7,25 @@ const LAST_CONTACT_UPDATE_INTERVAL = 250;
 // time taken for robot status to switch from "connected" to "disconnected" if no data is received (in ms)
 const DISCONNECT_THRESHOLD = 4000;
 
-const connectionDisplay = document.getElementById("connection-display");
-const lastContactTimeDisplay = document.getElementById("last-contact-time-display");
-const lastContactMsDisplay = document.getElementById("last-contact-ms-display");
-const batteryPercentageDisplay = document.getElementById("battery-percentage-display");
-const batteryVoltsDisplay = document.getElementById("battery-volts-display");
+const display = {
+	connection: document.getElementById("connection-display"),
+	lastContactTime: document.getElementById("last-contact-time-display"),
+	lastContactMs: document.getElementById("last-contact-ms-display"),
+	batteryPercentage: document.getElementById("battery-percentage-display"),
+	batteryVolts: document.getElementById("battery-volts-display"),
 
-const rightMotorSpeedDisplay = document.getElementById("right-motor-speed-display");
-const leftMotorSpeedDisplay = document.getElementById("left-motor-speed-display");
-const drivingDirectionDisplay = document.getElementById("driving-direction-display");
-const openerMotorStatusDisplay = document.getElementById("opener-motor-status-display");
-const pusherMotorStatusDisplay = document.getElementById("pusher-motor-status-display");
+	rightMotorSpeed: document.getElementById("right-motor-speed-display"),
+	leftMotorSpeed: document.getElementById("left-motor-speed-display"),
+	drivingDirection: document.getElementById("driving-direction-display"),
+	openerMotorStatus: document.getElementById("opener-motor-status-display"),
+	pusherMotorStatus: document.getElementById("pusher-motor-status-display"),
 
-const latencyDisplay = document.getElementById("latency-display");
-const currentTimeDisplay = document.getElementById("current-time-display");
+	detectedColour: document.getElementById("detected-colour-display"),
+	robotHeight: document.getElementById("robot-height-display"),
+
+	latency: document.getElementById("latency-display"),
+	currentTime: document.getElementById("current-time-display"),
+};
 
 let lastContactTimestamp = -Infinity;
 
@@ -28,17 +33,20 @@ const socket = io();
 socket.on("ev3-message", (rawData) => {
 	console.log(`Message received: ${rawData}`);
 
-	const data = JSON.parse(rawData);
-
 	lastContactTimestamp = Date.now();
 
-	batteryVoltsDisplay.textContent = round1(data.batteryLevel);
-	batteryPercentageDisplay.textContent = round1(
-		((data.batteryLevel - MIN_BATTERY_VOLTS) / FULL_BATTERY_VOLTS) * 100
+	const data = JSON.parse(rawData);
+
+	// python timestamp is in seconds
+	display.latency.textContent = Date.now() - data.timestamp;
+
+	display.batteryVolts.textContent = round1(data.batteryLevel);
+	display.batteryPercentage.textContent = round1(
+		((data.batteryLevel - MIN_BATTERY_VOLTS) / FULL_BATTERY_VOLTS) * 100,
 	);
 
-	leftMotorSpeedDisplay.textContent = data.leftMotorSpeed;
-	rightMotorSpeedDisplay.textContent = data.rightMotorSpeed;
+	display.leftMotorSpeed.textContent = data.leftMotorSpeed;
+	display.rightMotorSpeed.textContent = data.rightMotorSpeed;
 
 	let drivingDirection;
 
@@ -48,32 +56,35 @@ socket.on("ev3-message", (rawData) => {
 	else if (data.leftMotorSpeed < 0 && data.rightMotorSpeed > 0) drivingDirection = "left";
 	else if (data.leftMotorSpeed === 0 && data.rightMotorSpeed === 0) drivingDirection = "idle";
 	else drivingDirection = "???";
-	drivingDirectionDisplay.textContent = drivingDirection;
+	display.drivingDirection.textContent = drivingDirection;
 
-	openerMotorStatusDisplay.textContent = data.openerMotorSpeed === 0 ? "idle" : "opening";
-	pusherMotorStatusDisplay.textContent = data.pusherMotorSpeed === 0 ? "idle" : "pushing";
+	display.openerMotorStatus.textContent = data.openerMotorSpeed === 0 ? "idle" : "opening";
+	display.pusherMotorStatus.textContent = data.pusherMotorSpeed === 0 ? "idle" : "pushing";
+
+	display.detectedColour.textContent = data.detectedColour;
+	display.robotHeight.textContent = data.height;
 });
 
 setInterval(() => {
 	const lastContactDiff = Date.now() - lastContactTimestamp;
 	const lastContactDate = new Date(lastContactTimestamp);
 
-	lastContactTimeDisplay.textContent = formatTime(lastContactDate);
-	lastContactMsDisplay.textContent = String(lastContactDiff).padStart(4, "0");
+	display.lastContactTime.textContent = formatTime(lastContactDate);
+	display.lastContactMs.textContent = String(lastContactDiff).padStart(4, "0");
 
 	// if last contact was too long ago
 	if ((lastContactDiff > DISCONNECT_THRESHOLD)) {
 		// prevent rerunning if connectionDisplay already shows "disconnected"
-		if (connectionDisplay.textContent === "disconnected") return;
+		if (display.connection.textContent === "disconnected") return;
 
-		connectionDisplay.textContent = "disconnected";
+		display.connection.textContent = "disconnected";
 
 		for (const infoDisplay of document.getElementsByClassName("dynamic-info-display")) {
 			infoDisplay.style.color = "red";
 		}
 	// if lastContact is within threshold but connectionDisplay is still "disconnected" (robot reconnected)
-	} else if (connectionDisplay.textContent === "disconnected") {
-		connectionDisplay.textContent = "active";
+	} else if (display.connection.textContent === "disconnected") {
+		display.connection.textContent = "active";
 
 		for (const infoDisplay of document.getElementsByClassName("dynamic-info-display")) {
 			infoDisplay.style.color = "green";
@@ -82,7 +93,7 @@ setInterval(() => {
 }, LAST_CONTACT_UPDATE_INTERVAL);
 
 setInterval(() => {
-	currentTimeDisplay.textContent = formatTime(new Date(Date.now()));
+	display.currentTime.textContent = formatTime(new Date(Date.now()));
 }, 1000);
 
 /** rounds a number to 1 decimal place */
